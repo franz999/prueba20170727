36f521ef1f80a842d02be2f59b84ca6cdb87f422
ffernandezzabala@gmail.com <ffernandezzabala@gmail.com>
2017-07-27 16:40:10 -0300

    Se cambia salida por consola, por salida con Swing

--- Prueba20170727/src/Main.java
+++ Prueba20170727/src/Main.java
@@ -1,9 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated Ñmethod stub
-			System.out.println("Modifico proyecto externo");
+		// TODO Auto-generated method stub
+			//System.out.println("Modifico proyecto externo");
+		JOptionPane.showMessageDialog(null, "Bienvenido");
 	}
 
 }
