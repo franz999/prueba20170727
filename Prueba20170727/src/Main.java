e5639c6acee5e15709c6e905b697c4cebc777715
ffernandezzabala@gmail.com <ffernandezzabala@gmail.com>
2017-07-27 16:53:28 -0300

    Ahora hacemos también Ingreso con Swing

--- Prueba20170727/src/Main.java
+++ Prueba20170727/src/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			//System.out.println("Modifico proyecto externo");
-		JOptionPane.showMessageDialog(null, "Bienvenido");
+	    //System.out.println("Modifico proyecto externo");
+		
+		String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+		
+		JOptionPane.showMessageDialog(null, "Bienvenido " + nombre);
 	}
 
 }
